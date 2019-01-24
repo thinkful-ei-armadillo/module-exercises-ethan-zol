691a807076a295e9b337abcc7ef220b85427bbff
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-01-24 11:43:00 -0500

    Add Item.validateName() and Item.create()

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,8 +1,29 @@
+/* global cuid */
+
 'use strict';
 
 const Item = (function(){
+
   const foo = 'bar';
 
-  return {};
-}());
+  const validateName = function (name) {
 
+    if (!name) {
+      throw new TypeError('Name does not exist');
+    }
+  };
+
+  const create = function (name) {
+
+    return {
+      id      : cuid(),
+      name    : name,
+      checked : false,
+    };
+  };
+
+  return {
+    validateName,
+    create,
+  };
+}());
